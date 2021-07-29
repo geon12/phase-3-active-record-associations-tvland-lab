e0d1f02a02408758f1bad23e85bf3e1653fd8ca7
Cameron <camcreel@gmail.com>
2021-07-29 19:39:21 -0400

    Completed assignment

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,12 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
-    # Your code here
+    create_table :characters do |t|
+      # t.string :name 
+      # t.belongs_to :actor, foreign_key:true
+      # t.belongs_to :show, foreign_key:true
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+    end
   end
 end
